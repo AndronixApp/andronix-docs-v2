ba46814847dab61b877cafec2f05c90ace16edbe
Prakhar Shukla <imprakharshukla@gmail.com>
2023-05-13 19:16:40 +0530

    fix: rename blogs

--- next.config.js
+++ next.config.js
@@ -4,17 +4,17 @@ const withNextra = require('nextra')({
     async redirects() {
         return [
             {
-                source: 'blog/v7-0',
+                source: '/blog/v7-0/',
                 destination: '/blog/andronix-v7',
                 permanent: true,
             },
             {
-                source: 'blog/v6-0',
+                source: '/blog/v6-0/',
                 destination: '/blog/andronix-v6',
                 permanent: true,
             },
             {
-                source: 'blog/andronix-termux-and-f-droid/',
+                source: '/blog/andronix-termux-and-f-droid/',
                 destination: '/blog/andronix-termux-and-f-droid',
                 permanent: true,
             },
