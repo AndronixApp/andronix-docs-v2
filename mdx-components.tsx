ebfb01593ccbc920fe0b036c78ffa7f1c4909dc7
Prakhar Shukla <prakhars117@gmail.com>
2025-10-16 14:03:29 +0530

    mdx patch for r19

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,7 @@
+import type { MDXComponents } from 'mdx/types';
 import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 import defaultComponents from 'fumadocs-ui/mdx';
 import Imagera from '@/components/imagera';
-
-type MDXComponents = Record<string, React.ComponentType<any>>;
 
 export function useMDXComponents(components?: MDXComponents): MDXComponents {
   return {
